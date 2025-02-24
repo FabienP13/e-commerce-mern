cea5a8fa94ce5f0d0be906b486408d05745bb32d
Fabien PONCET <fponcet@centumadeneo.com>
2025-02-24 10:48:53 +0100

    [cartData] - implementing cartData backend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -41,6 +41,16 @@ const ShopContextProvider = (props) => {
             autoClose: 800,
         });
         setCartItems(cartData)
+
+        if (token) {
+            try {
+                await axios.post(backendUrl + '/api/cart/add', {itemId,size}, {headers:{token}})
+
+            } catch (error) {
+                console.log(error)
+                toast.error(error.message)
+            }
+        }
     }
 
     const getCartCount = () => {
@@ -53,7 +63,8 @@ const ShopContextProvider = (props) => {
                         totalCount += cartItems[items][item];
                     }
                 } catch (error) {
-                    
+                    console.log(error)
+                    toast.error(error.message)
                 }
             }
         }
@@ -65,6 +76,14 @@ const ShopContextProvider = (props) => {
         cartData[itemId][size] = quantity;
 
         setCartItems(cartData)
+
+        if (token) {
+            try {
+                await axios.post(backendUrl + '/api/cart/update', {itemId,size,quantity}, {headers:{token}})
+            } catch (error) {
+                
+            }
+        }
     }
 
     const getCartAmount = () => {
@@ -99,6 +118,19 @@ const ShopContextProvider = (props) => {
         }
     }
 
+    const getUserCart = async (token) => {
+        try {
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, {headers: {token}})
+
+            if(response.data.success) {
+                setCartItems(response.data.cartData)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     useEffect(()=>{
         getProductsData()
     }, [])
@@ -106,6 +138,7 @@ const ShopContextProvider = (props) => {
     useEffect(()=>{
         if (!token && localStorage.getItem("token")) {
             setToken(localStorage.getItem("token"))
+            getUserCart(localStorage.getItem("token"))
         }
     },[])
 
